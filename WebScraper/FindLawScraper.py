81a3b5711d427ea06e9e1b0df6bc4ea2aad8f767
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-06-11 22:21:03 -0400

    get cases

--- WebScraper/FindLawScraper.py
+++ WebScraper/FindLawScraper.py
@@ -8,15 +8,35 @@ import threading
 import time
 
 MAX_WORKERS = 50
+TITLE_XPATH = "/html/body/div[2]/section/div[1]/div[4]/div/center/h3[1]/p"
+BODY_XPATH = "/html/body/div[2]/section/div[1]/div[4]/div"
 
 def get_case_links():
+    driver = webdriver.Chrome(options=chrome_options)
     while len(year_links) > 0:
         link = year_links.pop()
-        driver = webdriver.Chrome(options=chrome_options)
         driver.get(link)
         case_links_year = driver.find_elements("tag name", "tbody")[3].find_elements("tag name", "a")
         case_links_year = [link.get_attribute("href") for link in case_links_year]
         case_links.extend(case_links_year)
+        break
+    driver.quit()
+
+def get_case():
+    driver = webdriver.Chrome(options=chrome_options)
+    case_links = ["https://caselaw.findlaw.com/court/us-supreme-court/22-148.html"]
+    while len(case_links) > 0:
+        link = case_links.pop()
+        driver.get(link)
+        title = driver.find_element("xpath", TITLE_XPATH).text
+        body = driver.find_element("xpath", BODY_XPATH).text
+        body_list = body.split("\n")
+        body_list = [line.strip() for line in body_list if " " in line.strip()]
+        with open(f"../SupremeCourtCases/{title}.txt", "w") as f:
+            f.write("\n".join(body_list[1:]))
+        break
+    driver.quit()
+
 
 if __name__ == "__main__":
     chrome_options = webdriver.ChromeOptions()
@@ -35,9 +55,9 @@ if __name__ == "__main__":
     # get case links in parallel
     case_links = []
     pool = concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS)
-    futures = [pool.submit(get_case_links) for _ in range(MAX_WORKERS)]
+    link_futures = [pool.submit(get_case_links) for _ in range(MAX_WORKERS)]
+    concurrent.futures.wait(link_futures, timeout=None,return_when=concurrent.futures.FIRST_COMPLETED)
 
-    concurrent.futures.wait(futures, timeout=None,return_when=concurrent.futures.FIRST_COMPLETED)
-    print(len(case_links))
+    # get cases in parallel
+    case_futures = [pool.submit(get_case) for _ in range(MAX_WORKERS)]
     pool.shutdown(wait=True)
-    print(len(case_links))
